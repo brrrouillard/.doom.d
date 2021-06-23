56a037dfc9b2b1d8ea689700a0c3243f4a3a327d
matthieub <matthieub@LMBEL-037.local>
2021-06-23 20:58:49 +0200

    Update config

--- config.el
+++ config.el
@@ -53,3 +53,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+
+;; https://github.com/hlissner/doom-emacs/issues/2068
+(setq-hook! 'js2-mode-hook flycheck-checker 'javascript-eslint)
